2f920eee2d5ff22312a115a8ad18be4e8e29c5bd
rohit verma <rohit9721382965@gmail.com>
2020-10-31 20:26:11 +0530

    added

--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.core.paginator import Paginator
 
 from main.models import Post
+from main.forms import Contactform
 
 
 # Create your views here.
@@ -9,5 +11,24 @@ def index(request):
 
 def blog(request):
     posts = Post.objects.all()
-    context = {'posts': posts}
+    paginator = Paginator(posts, 2)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    
+    context = {'page_obj': page_obj}
     return render(request, 'main/blog.html', context)
+
+def post(request, post_id=None):
+    post=Post.objects.get(id=post_id)
+    context = {'post': post}
+    return render(request, 'main/post.html', context)
+
+def contact(request):
+    form =Contactform(request.POST or None)
+    if form.is_valid():
+        form.save()
+
+        return redirect('contact')
+    context={'form':form}
+    return render(request, 'main/contact.html',context)
